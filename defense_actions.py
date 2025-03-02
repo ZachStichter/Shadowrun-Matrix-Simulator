450c8a6f7831a53f54e63d8d828eda3cde674b80
Zach Stichter <zstichter@gmail.com>
2025-03-02 13:16:05 -0500

    created defense_actions

---
+++ defense_actions.py
@@ -0,0 +1,8 @@
+import common_functions
+def system_sweep(active_utils,*args):
+    print('sweeping system (this is a test line)')
+    get_bonus('mirrors',active_utils)
+
+DEFENSE_ACTIONS_DICTIONARY={
+    'system_sweep':system_sweep
+}
