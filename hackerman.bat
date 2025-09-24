7ffc37ccdc82490a1a59a7d731f14671a334ec8b
ZachStichter <zstichter@gmail.com>
2025-09-24 09:11:08 -0400

    update .gitingore

--- hackerman.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Psoft Matrix Engine v0.0.1
-py "C:\Users\Zach\OneDrive - Western Kentucky University\_Actual Documents\research\scripts\250201-shadowrun-matrix-emulator\main.py"
-exit
